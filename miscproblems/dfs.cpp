8f40cab00406f955bb1fc2d0202d5b0606d92d3c
vanyusuf1 <yusufvan71@gmail.com>
2021-01-01 11:30:25 +0530

    dfs

---
+++ miscproblems/dfs.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<list>
+#include<map>
+#include<queue>
+using namespace std;
+
+template<typename T>
+class Graph {
+	map<T, list<T> > l;
+public:
+	void addEdge(int x, int y) {
+		l[x].push_back(y);
+		l[y].push_back(x);
+
+
+	}
+	void dfs_helper(T src, map<T, bool> &visited) {
+		cout << src << " ";
+		visited[src] = true;
+		for (T nbr : l[src]) {
+			if (!visited[nbr]) {
+				dfs_helper(nbr, visited);
+			}
+		}
+	}
+
+	void dfs(T src) {
+		map<T, bool> visited;
+		for (auto p : l) {
+			T node = p.first;
+			visited[node] = false;
+		}
+		dfs_helper(src, visited);
+	}
+	void topological_sort(){
+		int *indegree = new int[V];
+		for(int i = 0; i < V;i++){
+			indegree[i] =0;
+		}
+		for(auto p :l){
+			int x=p.first;
+			for(auto y:p.second){
+				indegree[y]++;
+
+			}
+		}
+	}
+
+
+};
+
+
+int main() {
+	Graph<int> g;
+	g.addEdge(0, 1);
+	g.addEdge(1, 2);
+	g.addEdge(2, 3);
+	g.addEdge(3, 4);
+	g.addEdge(4, 5);
+
+	g.dfs(0);
+
+	return 0;
+}
